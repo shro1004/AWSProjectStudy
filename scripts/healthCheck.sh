f6557db23a2cdbae5c21dbc4b9878bc9450170f1
김연균 <shro1004@gmail.com>
2019-03-06 17:23:06 +0900

    healthCheck 수정

--- scripts/healthCheck.sh
+++ scripts/healthCheck.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 echo "> Health check 시작"
-echo "> curl -s http://localhost:8080/actuator/health "
 
 for RETRY_COUNT in {1..15}
 do
-  RESPONSE=$(curl -s http://localhost:8080/actuator/health)
-  UP_COUNT=$(echo $RESPONSE | grep 'UP' | wc -l)
+  RESPONSE1=$(curl -s http://localhost:8081/actuator/health)
+  UP_COUNT1=$(echo $RESPONSE1 | grep 'UP' | wc -l)
+  
+  RESPONSE2=$(curl -s http://localhost:8082/actuator/health)
+  UP_COUNT2=$(echo $RESPONSE2 | grep 'UP' | wc -l)
 
-  if [ $UP_COUNT -ge 1 ]
+  if [ $UP_COUNT1 -ge 1 ]
   then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
       echo "> Health check 성공"
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
-      echo "> Health check: ${RESPONSE}"
+      echo "> Health check: ${RESPONSE1}"
+  fi
+  
+  if [ $UP_COUNT2 -ge 1 ]
+  then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      break
+  else
+      echo "> Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
+      echo "> Health check: ${RESPONSE2}"
   fi
 
   if [ $RETRY_COUNT -eq 10 ]
